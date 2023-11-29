39df0e72367bdc6beab6fd5a717bad1406deea3e
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-11-29 13:06:25 +0300

    Before interface segregation

--- src/Mail/IEmailSendable.java
+++ src/Mail/IEmailSendable.java
@@ -2,7 +2,10 @@ package Mail;
 
 import Driver.DriverInfo;
 
+import java.util.List;
+
 public interface IEmailSendable {
     void sendTripInfoMailToDriver(DriverInfo driver);
+    void sendTripInfoMailToDrivers(List<DriverInfo> driver);
 }
 
